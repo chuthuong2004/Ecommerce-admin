e64535029bf2513359c423f1881ca969b1e5207c
Nguyễn Quốc Trọn <90500313+QuocTron@users.noreply.github.com>
2022-11-10 01:10:23 +0700

    dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,7 +4,22 @@ import React, { useEffect, ReactNode } from 'react';
 const cx = classNames.bind(styles);
 
 const Dashboard = () => {
-  return <div className={cx('container')}>Dashboard</div>;
+  return (
+    <div className={cx('container')}>
+      <div className={cx('content')}>
+        <div className={cx('container-chart')}>
+          <div className={cx('sales-chart')}>
+            <span className={cx('title')}>Sales Chart</span>
+            <div className={cx('chart')}> Chart</div>
+          </div>
+          <div className={cx('channel')}>
+            <span className={cx('title')}>Channels</span>
+            <div className={cx('channel-chart')}>Channels</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
