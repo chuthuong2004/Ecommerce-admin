a27d5ded48f138c20777098bda0ec65357c47e9b
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2022-11-10 16:11:15 +0700

    12312312

--- src/App.tsx
+++ src/App.tsx
@@ -28,21 +28,25 @@ const App: React.FC = () => {
         {routes.map((route, index) => {
           const Page = route.component;
           let Layout: any = DefaultLayout;
+          let PrivateRoute = RequireAuth;
           if (route.layout) {
             Layout = route.layout;
           } else if (route.layout === null) {
             Layout = Fragment;
+            PrivateRoute = ({ children }: { children: any }) => {
+              return children;
+            };
           }
           return (
             <Route
               key={index}
               path={route.path}
               element={
-                <RequireAuth>
+                <PrivateRoute>
                   <Layout>
                     <Page />
                   </Layout>
-                </RequireAuth>
+                </PrivateRoute>
               }
             />
           );
